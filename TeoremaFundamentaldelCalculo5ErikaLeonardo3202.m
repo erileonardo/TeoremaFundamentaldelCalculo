d92404fb4280917760c6dab255b27a3fdddc8378
erileonardo <82676091+erileonardo@users.noreply.github.com>
2021-05-27 23:51:20 -0500

    Create TeoremaFundamentaldelCalculo5ErikaLeonardo3202.m

---
+++ TeoremaFundamentaldelCalculo5ErikaLeonardo3202.m
@@ -0,0 +1,16 @@
+% Octave Script
+% Title       : Teorema Fundamental del Calculo
+% Description : Encontrar la antiderivada de funcion definida en un [a,b]  
+% Author      : Erika Amairani Leonardo Arce
+% Date        : 20 de Mayo 2021
+% Version     : 1
+% Notes       : funcion f(x)=(√a-√x)^2
+
+clear all
+clc
+syms x
+syms a
+f=(sqrt(a)-sqrt(x)).^2
+a=0
+b=a
+integral=int(f,x,a,[a,b])
