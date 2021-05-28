f421d153db5a620e314c59c0e3bae9478a8f830d
erileonardo <82676091+erileonardo@users.noreply.github.com>
2021-05-27 23:40:22 -0500

    Create TeoremaFundamentaldelCalculo3ErikaLeonardo3202.m

---
+++ TeoremaFundamentaldelCalculo3ErikaLeonardo3202.m
@@ -0,0 +1,29 @@
+% Octave Script
+% Title       : Teorema Fundamental del Calculo
+% Description : Encontrar la antiderivada de funcion definida en un [a,b]  
+% Author      : Erika Amairani Leonardo Arce
+% Date        : 20 de Mayo 2021
+% Version     : 1
+% Notes       : funcion f(x)= ?2x+3?x
+
+clear all
+clc
+syms x
+
+f=input('Ingresar funcion: ')
+a=input('Ingresar intervalo inferior: ')
+b=input('Ingresar intervalo superior: ')
+integral=int(f,x,a,b)
+
+x=0:0.1:8;
+%y=sqrt(2*x);%+(x.^1/3);
+y=sqrt(2*x)+(x.^(1/3));
+% Dibujar grafica
+area(x,y)
+grid on; 
+% Titulo
+title("f(x)=?2x+3?x");
+% Etiqueta para x
+xlabel("X");
+% Etiqueta para y
+ylabel("Y");
