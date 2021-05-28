80e832e5912cad89b4159c164dc6e83b020c43eb
erileonardo <82676091+erileonardo@users.noreply.github.com>
2021-05-28 00:03:21 -0500

    Update TeoremaFundamentaldelCalculo4ErikaLeonardo3202.m

--- TeoremaFundamentaldelCalculo4ErikaLeonardo3202.m
+++ TeoremaFundamentaldelCalculo4ErikaLeonardo3202.m
@@ -2,7 +2,7 @@
 % Title       : Teorema Fundamental del Calculo
 % Description : Encontrar la antiderivada de funcion definida en un [a,b]  
 % Author      : Erika Amairani Leonardo Arce
-% Date        : 20 de Mayo 2021
+% Date        : 27 de Mayo 2021
 % Version     : 1
 % Notes       : funcion f(y)= 1+?y/y^2
 
