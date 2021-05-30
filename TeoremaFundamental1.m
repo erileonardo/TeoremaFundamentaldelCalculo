2767627ca18777e9ba01fd44cf3bc419e7d085e4
erileonardo <82676091+erileonardo@users.noreply.github.com>
2021-05-29 22:06:11 -0500

    Add files via upload

---
+++ TeoremaFundamental1.m
@@ -0,0 +1,27 @@
+% Octave Script
+% Title       : Teorema Fundamental del Calculo
+% Description : Encontrar la antiderivada de funcion definida en un [a,b]  
+% Author      : Erika Amairani Leonardo Arce
+% Date        : 27 de Mayo 2021
+% Version     : 1
+% Notes       : funcion f(x)=x^3
+
+clear all
+clc
+syms x
+f=input('Ingresar funcion: ')
+a=input('Ingresar intervalo inferior: ')
+b=input('Ingresar intervalo superior: ')
+integral=int(f,x,a,b)
+
+x=-1:0.1:3;
+y=x.^3;
+% Dibujar grafica
+area(x,y)
+grid on; 
+% Titulo
+title("f(x)=x^3");
+% Etiqueta para x
+xlabel("X");
+% Etiqueta para y
+ylabel("Y");
